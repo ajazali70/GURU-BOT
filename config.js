4b7d78ce7030ea76fbfee6b67602bb2b235af633
ajazali70 <154875692+ajazali70@users.noreply.github.com>
2023-12-26 13:28:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+923123068202', 'Ajaz Ali', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
